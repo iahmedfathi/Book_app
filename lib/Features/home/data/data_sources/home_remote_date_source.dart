b5d59e782732ce5c9c15c17938b05ebdb985c0a6
ahmedfathi <ah0640658@gmail.com>
2025-04-06 00:30:55 +0200

    create home repo impl

--- lib/Features/home/data/data_sources/home_remote_date_source.dart
+++ lib/Features/home/data/data_sources/home_remote_date_source.dart
@@ -3,13 +3,12 @@ import 'package:bookly/Features/home/domain/entites/book_entity.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/api/api_consumer.dart';
 import 'package:bookly/core/api/end_pointes.dart';
-import 'package:bookly/core/errors/exceptions.dart';
 import 'package:bookly/core/functions/save_box_data.dart';
-import 'package:dartz/dartz.dart';
+
 
 abstract class HOmeRemoteDateSource {
-  Future<Either<String, List<BookEntity>>> fetchRemoteFeaturedBooks();
-  Future<Either<String, List<BookEntity>>> fetchRemoteNewestBooks();
+  Future<List<BookEntity>> fetchRemoteFeaturedBooks();
+  Future<List<BookEntity>> fetchRemoteNewestBooks();
 }
 
 class HomeRemoteDateSourceImpl extends HOmeRemoteDateSource {
@@ -17,35 +16,27 @@ class HomeRemoteDateSourceImpl extends HOmeRemoteDateSource {
 
   HomeRemoteDateSourceImpl({required this.apiConsumer});
   @override
-  Future<Either<String, List<BookEntity>>> fetchRemoteFeaturedBooks() async {
-    try {
-      final response = await apiConsumer.get(EndPoint.featuredBookEndPoint);
-      List<BookEntity> books = getBooksList(response);
-      saveBoxData(books, kFeaturedBooksBox);
-      return Right(books);
-    } on ServerException catch (e) {
-      return Left(e.errModel.errorMessage);
-    }
+  Future<List<BookEntity>> fetchRemoteFeaturedBooks() async {
+    final response = await apiConsumer.get(EndPoint.featuredBookEndPoint);
+    List<BookEntity> books = getBooksList(response);
+    saveBoxData(books, kFeaturedBooksBox);
+    return books;
   }
 
   @override
-  Future<Either<String, List<BookEntity>>> fetchRemoteNewestBooks() async {
-    try {
-      final response = await apiConsumer.get(EndPoint.newestBookEndPoint);
-      List<BookEntity> books = getBooksList(response);
-      saveBoxData(books, kNewestBooksBox);
-
-      return Right(books);
-    } on ServerException catch (e) {
-      return Left(e.errModel.errorMessage);
-    }
-  }
-
-  List<BookEntity> getBooksList(response) {
-    List<BookEntity> books = [];
-    for (var book in response["items"]) {
-      books.add(BookModel.fromJson(book));
-    }
+  Future<List<BookEntity>> fetchRemoteNewestBooks() async {
+    final response = await apiConsumer.get(EndPoint.featuredBookEndPoint);
+    List<BookEntity> books = getBooksList(response);
+    saveBoxData(books, kFeaturedBooksBox);
     return books;
   }
+List<BookEntity> getBooksList(response) {
+  List<BookEntity> books = [];
+  for (var book in response["items"]) {
+    books.add(BookModel.fromJson(book));
+  }
+  return books;
 }
+}
+
+
--- lib/Features/home/data/repo/home_repo_impl.dart
+++ lib/Features/home/data/repo/home_repo_impl.dart
@@ -1,3 +1,46 @@
+import 'package:bookly/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly/Features/home/data/data_sources/home_remote_date_source.dart';
+import 'package:bookly/Features/home/domain/entites/book_entity.dart';
+import 'package:bookly/Features/home/domain/repo/hone_repo.dart';
+import 'package:bookly/core/errors/exceptions.dart';
+import 'package:dartz/dartz.dart';
 
+class HomeRepoImpl extends HomeRepo {
+  final HOmeRemoteDateSource homeRemoteDateSource;
+  final HomeLocalDataSource homeLocalDateSource;
 
+  HomeRepoImpl(
+      {required this.homeRemoteDateSource, required this.homeLocalDateSource});
+  @override
+  Future<Either<String, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = await homeLocalDateSource.fetchLocalFeaturedBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      }
+      books = await homeRemoteDateSource.fetchRemoteFeaturedBooks();
 
+      return Right(books);
+    } on ServerException catch (e) {
+      return Left(e.errModel.errorMessage);
+    }
+  }
+
+  @override
+  Future<Either<String, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = await homeLocalDateSource.fetchLocalNewestBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      }
+      books = await homeRemoteDateSource.fetchRemoteNewestBooks();
+
+      return Right(books);
+    } on ServerException catch (e) {
+      return Left(e.errModel.errorMessage);
+    }
+    
+  }
+}
