e42658b189cd31ff758be67f499bd8c5dc30283e
ahmedfathi <ah0640658@gmail.com>
2025-04-06 21:42:05 +0200

    make multi bloc providers

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,20 +1,31 @@
-
-
 // Create an instance of GetIt
+import 'package:bookly/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly/Features/home/data/data_sources/home_remote_date_source.dart';
+import 'package:bookly/Features/home/data/repo/home_repo_impl.dart';
+import 'package:bookly/Features/home/domain/repo/hone_repo.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featured_book_use_case.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 import 'package:bookly/core/api/api_consumer.dart';
 import 'package:bookly/core/api/dio_consumer.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-final GetIt  locator = GetIt.instance;
+final GetIt locator = GetIt.instance;
 
 void setupServiceLocator() {
   locator.registerSingleton<DioConsumer>(DioConsumer(dio: Dio()));
   // Register API Consumer
   locator.registerSingleton<ApiConsumer>(locator.get<DioConsumer>());
+  locator.registerSingleton<HomeRepo>(
+    HomeRepoImpl(
+      homeLocalDateSource: HomeLocalDataSourceImpl(),
+      homeRemoteDateSource:
+          HomeRemoteDateSourceImpl(apiConsumer: locator.get<ApiConsumer>()),
+    ),
+  );
 
- 
-  
-
-  // Register Auth Cubit
+  locator.registerSingleton<FetchFeaturedBookUseCase>(
+      FetchFeaturedBookUseCase(homeRepo: locator.get<HomeRepo>()));
+  locator.registerSingleton<FetchNewestBooksUseCase>(
+      FetchNewestBooksUseCase(homeRepo: locator.get<HomeRepo>()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:bookly/Features/home/domain/entites/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featured_book_use_case.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/manger/newest_bools_cubit/newest_books_cubit.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -19,12 +25,27 @@ class BookApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FeaturedBooksCubit>(
+          create: (context) =>
+              FeaturedBooksCubit(locator.get<FetchFeaturedBookUseCase>())
+                ..fetchFeaturedBooks(),
+        ),
+        BlocProvider<NewestBooksCubit>(
+          create: (context) =>
+              NewestBooksCubit(locator.get<FetchNewestBooksUseCase>())
+                ..fetchNewestBooks(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
